6a4f721801ba979c958e66fa019d436f937eefb7
btierney <bob.tierney@verizon.net>
2018-10-22 14:54:35 -0500

    Red Hat Office Phone Numbers
    These inserts relate to the locations table which contain a few RED HAT offices world wide

--- projects/location-soap/src/main/resources/schema.sql
+++ projects/location-soap/src/main/resources/schema.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS location_detail
+
 CREATE TABLE location_detail (
   id integer NOT NULL,
   phone varchar(30) NOT NULL,
@@ -5,5 +7,15 @@ CREATE TABLE location_detail (
   operating_hour varchar(200) NOT NULL
 );
 
-INSERT INTO location_detail (id,phone,owner,operating_hour) VALUES (1,'345-678-9567','XXX XXX', '8:00 AM - 5:00 PM');
-INSERT INTO location_detail (id,phone,owner,operating_hour) VALUES (2,'546-232-7467','XXX XXX', '9:00 AM - 6:00 PM');
+INSERT INTO location_detail (id,phone,owner,operating_hour) VALUES (1,'+27 11 513 4109','XXX XXX', '8:00 AM - 5:00 PM');
+INSERT INTO location_detail (id,phone,owner,operating_hour) VALUES (2,'+1 978 392 1000','XXX XXX', '9:00 AM - 6:00 PM');
+INSERT INTO location_detail (id,phone,owner,operating_hour) VALUES (3,'+971 4 449 4101','XXX XXX', '9:00 AM - 6:00 PM');
+INSERT INTO location_detail (id,phone,owner,operating_hour) VALUES (4,'+34 932 204 205','XXX XXX', '9:00 AM - 6:00 PM');
+INSERT INTO location_detail (id,phone,owner,operating_hour) VALUES (5,'+1 919 754 4950','XXX XXX', '9:00 AM - 6:00 PM');
+INSERT INTO location_detail (id,phone,owner,operating_hour) VALUES (6,'+52 55 8851 6400','XXX XXX', '9:00 AM - 6:00 PM');
+INSERT INTO location_detail (id,phone,owner,operating_hour) VALUES (7,'+1 650 567 9039','XXX XXX', '9:00 AM - 6:00 PM');
+INSERT INTO location_detail (id,phone,owner,operating_hour) VALUES (8,'+44 20 7009 4444','XXX XXX', '9:00 AM - 6:00 PM');
+INSERT INTO location_detail (id,phone,owner,operating_hour) VALUES (9,'+61 2 8923 2800','XXX XXX', '9:00 AM - 6:00 PM');
+INSERT INTO location_detail (id,phone,owner,operating_hour) VALUES (10,'+91 11 66442029/37','XXX XXX', '9:00 AM - 6:00 PM');
+INSERT INTO location_detail (id,phone,owner,operating_hour) VALUES (11,'+55 21 2546-9804','XXX XXX', '9:00 AM - 6:00 PM');
+INSERT INTO location_detail (id,phone,owner,operating_hour) VALUES (12,'+81 3 5798 8500','XXX XXX', '9:00 AM - 6:00 PM');
