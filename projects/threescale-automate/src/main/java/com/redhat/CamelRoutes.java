e5702beabdb2a930ccdb3b2164f930c695c02f34
craig <cbrookes@redhat.com>
2019-02-15 13:54:15 +0000

    fix for microcks issue and camel route issue

--- projects/threescale-automate/src/main/java/com/redhat/CamelRoutes.java
+++ projects/threescale-automate/src/main/java/com/redhat/CamelRoutes.java
@@ -77,7 +77,7 @@ public class CamelRoutes extends RouteBuilder {
 				.setHeader(Exchange.HTTP_METHOD, constant("POST"))
 				.setHeader(Exchange.CONTENT_TYPE, constant(MediaType.APPLICATION_FORM_URLENCODED_VALUE))
 				.setBody(simple("username={{env:SSO_USERNAME}}&password={{env:SSO_PASSWORD}}&grant_type=password&client_id=admin-cli"))
-			.toD("https4://secure-sso-sso.${headers.openshiftappurl}/auth/realms/master/protocol/openid-connect/token?sslContextParameters=#ssl&bridgeEndpoint=true")
+			.toD("https4://sso-sso.${headers.openshiftappurl}/auth/realms/master/protocol/openid-connect/token?sslContextParameters=#ssl&bridgeEndpoint=true")
 			.setHeader("tkn").jsonpath("access_token")
 			//.log("return---->  ${header.tkn}")	
 		
@@ -85,7 +85,7 @@ public class CamelRoutes extends RouteBuilder {
 			//GET Client ID
 				.setHeader(Exchange.HTTP_METHOD, constant("GET"))
 				.setHeader("Authorization").simple("Bearer ${headers.tkn}")
-			.toD("https4://secure-sso-sso.${headers.openshiftappurl}/auth/admin/realms/${headers.userid}/clients?sslContextParameters=#ssl&bridgeEndpoint=true")
+			.toD("https4://sso-sso.${headers.openshiftappurl}/auth/admin/realms/${headers.userid}/clients?sslContextParameters=#ssl&bridgeEndpoint=true")
 			.setHeader("idClientAdmin").jsonpath("$..[?(@.clientId == '3scale-admin')].id")
 			//.log("return---->  ${headers.idClientAdmin}")	
 			
@@ -93,7 +93,7 @@ public class CamelRoutes extends RouteBuilder {
 			//GET Client Secret
 				.setHeader(Exchange.HTTP_METHOD, constant("GET"))
 				.setHeader("Authorization").simple("Bearer ${headers.tkn}")
-			.toD("https4://secure-sso-sso.${headers.openshiftappurl}/auth/admin/realms/${headers.userid}/clients/${headers.idClientAdmin}/client-secret?sslContextParameters=#ssl&bridgeEndpoint=true")
+			.toD("https4://sso-sso.${headers.openshiftappurl}/auth/admin/realms/${headers.userid}/clients/${headers.idClientAdmin}/client-secret?sslContextParameters=#ssl&bridgeEndpoint=true")
 			.setHeader("secret").jsonpath("value")
 			.log("return---->  ${headers.secret}")	
 		
